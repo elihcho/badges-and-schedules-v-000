410b7b6674054a36a3b2f02601d6d743228b372e
Eli Cho <eli.cho@gmail.com>
2018-07-26 05:39:01 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,10 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
 def batch_badge_creator(array)
-  
+  array.each do |name|
+    badge_maker(name)
+  end
 end
